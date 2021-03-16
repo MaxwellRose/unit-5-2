d73e4a04121d1f5c10ff2567dd3bac7e0791ebd0
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-16 16:29:19 +0000

    more errors

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-document.getElementById('button1').addEventListener("click", myFunction)
+document.getElementById('button1').addEventListener('click', myFunction)
 
 function myFunction () {
   document.getElementById('button1').innerHTML = 'Good Job'
-} /*this makes the text change*/
+} /* this makes the text change */
 
 document.getElementById('button2').addEventListener('click', myFunction1)
 
 function myFunction1 () {
   document.getElementById('button2').style.visibility = 'hidden'
-} /*this makes the button disappear*/
+} /* this makes the button disappear */
 
 document.getElementById('button3').addEventListener('click', myFunction2)
 

1278578d37b1a35b6e7af5e1d7e0d1666f8a8e58
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-16 16:12:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("button1").addEventListener("click", myFunction)
+
+function myFunction() {
+  document.getElementById("button1").innerHTML = "Good Job"
+}/*this makes the text change*/
+
+document.getElementById("button2").addEventListener("click", myFunction1)
+
+function myFunction1() {
+  document.getElementById("button2").style.visibility = "hidden"
+}/*this makes the button disappear*/
+
+document.getElementById("button3").addEventListener("click", myFunction2)
+
+function myFunction2() {
+  document.getElementById("button3").style.background = "red"
+}
